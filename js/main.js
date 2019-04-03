cc0bc40a2994d01bd5c8ec5d35fe38f0575d977c
DP <dushanperic@gmail.com>
2019-04-03 18:25:15 +0200

    portfolio first upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var i = 0;
+var txt = 'Web Design & Development';
+var speed = 75;
+
+	function typeWriter() {
+	  if (i < txt.length) {
+	    document.getElementById("demo").innerHTML += txt.charAt(i);
+	    i++;
+	    setTimeout(typeWriter, speed);
+	  }
+	}
+(function() {
+  typeWriter();
+
+})();
+
+
+
+$(function(){
+	$('.header-link').click(function(){
+		$('#about-popup').addClass('active')
+	});
+
+	$('.pop-btn').click(function(){
+		$('#about-popup').removeClass('active')
+	});
+});
+
+$(document).on('mousemove', function(e) {
+	 var xPos = e.pageX;
+	 var yPos = e.pageY;
+	 $('.list-item--img').css({
+	   'top': yPos - 100 + 'px',
+	   'left': xPos + 130 + 'px'
+	 });
+});
