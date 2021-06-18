4931b96968ea1bc981b11cb5444eee81df582d98
Dusan Peric <dushanperic1@gmail.com>
2021-06-19 01:20:19 +0200

    Update list

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 var i = 0;
-var txt = 'Web Design & Development';
+var txt = "Web Development";
 var speed = 75;
 
-	function typeWriter() {
-	  if (i < txt.length) {
-	    document.getElementById("demo").innerHTML += txt.charAt(i);
-	    i++;
-	    setTimeout(typeWriter, speed);
-	  }
-	}
-(function() {
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+(function () {
   typeWriter();
-
 })();
 
+$(function () {
+  $(".header-link").click(function () {
+    $("#about-popup").addClass("active");
+  });
 
-
-$(function(){
-	$('.header-link').click(function(){
-		$('#about-popup').addClass('active')
-	});
-
-	$('.pop-btn').click(function(){
-		$('#about-popup').removeClass('active')
-	});
+  $(".pop-btn").click(function () {
+    $("#about-popup").removeClass("active");
+  });
 });
 
-$(document).on('mousemove', function(e) {
-	 var xPos = e.pageX;
-	 var yPos = e.pageY;
-	 $('.list-item--img').css({
-	   'top': yPos - 100 + 'px',
-	   'left': xPos + 130 + 'px'
-	 });
+$(document).on("mousemove", function (e) {
+  var xPos = e.pageX;
+  var yPos = e.pageY;
+  $(".list-item--img").css({
+    top: yPos - 100 + "px",
+    left: xPos + 130 + "px",
+  });
 });
